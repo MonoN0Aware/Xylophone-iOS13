e7a49a0c427d66c14a45cffb2a7573ccd02374ea
Boluwatife Adesina <badesina82@gmail.com>
2021-04-24 16:38:37 +0100

    I'm learning

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playNote()
+    }
     
-
+    func playNote() {
+        // need to declare local path as url
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        // now use declared path 'url' to initialize the player
+        player = AVPlayer.init(url: url!)
+        // after initialization play audio its just like click on play button
+        player!.play()
+    }
+    
 }
 
